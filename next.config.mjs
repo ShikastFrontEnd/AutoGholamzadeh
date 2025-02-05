2ada3f2e696440a0c19d1529371b0863ab349083
ShikastDev <ShikastDev@gmail.com>
2025-02-05 16:14:35 +0330

    Bug Fix In Next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,18 +3,4 @@ export const env = {
     BASE_URL: process.env.BASE_URL || 'https://api.gholamzadeh.com',
 };
 
-/** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     output: "standalone",
-//     images: {
-//         remotePatterns: [
-//             {
-//                 protocol: 'https',
-//                 // hostname:'https://api.gholamzadeh.com',
-//                 // hostname: 'http://192.168.1.103:8000',
-//                 // pathname: '/**',
-//             },
-//         ],
-//     },
-// }
-// module.exports = nextConfig
+
