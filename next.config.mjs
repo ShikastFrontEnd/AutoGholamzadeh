6e122a4267b105a3cc2a891df8dc2a3bee471662
ShikastDev <ShikastDev@gmail.com>
2025-02-05 16:17:45 +0330

    Test Bug Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 // next.config.js
+import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
+
 export const env = {
     BASE_URL: process.env.BASE_URL || 'https://api.gholamzadeh.com',
 };
+
+
 
 
