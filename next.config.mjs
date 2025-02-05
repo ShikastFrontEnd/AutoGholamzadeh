112a543bc63d528e6c0322a5ffc78c5c0057a369
ShikastDev <ShikastDev@gmail.com>
2025-02-05 16:19:40 +0330

    Tes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 // next.config.js
-import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
-
 export const env = {
     BASE_URL: process.env.BASE_URL || 'https://api.gholamzadeh.com',
 };
