d6794dd8c927901b83eb7fe7ef0cee4c4d16fc1d
ALiSAKUNA <alinikooobidel@gmail.com>
2025-02-18 11:43:30 +0330

    admin/panel added to the permenet redirect

---
+++ src/app/admin/panel/page.jsx
@@ -0,0 +1,6 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function adminpanel() {
+    // Permanent redirect to the specified URL
+    permanentRedirect("https://api.gholamzadeh.com/");
+}
